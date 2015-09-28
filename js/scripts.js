d8448be0e1443a20bc70bd94ed3abbfbc2f689be
Maile Lucks <maile.lucks@gmail.com>
2015-09-28 13:23:04 -0700

    Making Items Delete

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// $(document).ready(function() {
+// $('document').ready(function(){
+
+/* Allows user to add list items */
+	function addListItem(){
+		var itemName = $('#itemName').val();
+		var itemPrice = $('#price').val();
+		var itemQuantity = $('#quantity').val();
+		var itemRetailer = $('#retailer').val();
+		var itemRow = $('<div class="item"><ul class="item-details"><li>' + itemName + '</li><li>' + itemPrice +'</li><li>' + itemQuantity + '</li><li>' + itemRetailer + '</li></ul><ul class="options"><li class="check"><input class="check" type="checkbox" name="checked"></li><li class="switch"><input id="toggle" class="toggle toggle-round" type="checkbox"><label for="toggle"></label>	</li><li class="delete"><button id="delete">X</button></li></ul></div>' );
+
+		$('.shop-list').prepend(itemRow);
+    	$('itemName').val();
+	}
+
+	/* Allows user to delete list items */
+function deleteItem() {
+
+    $(this).parent().parent().remove();
+}
+
+	/* Adds item to list */
+$(function() {
+
+    $('#addItem').on('click', addListItem);
+    // $('#itemList').on('click', '.checkbox', tickItem);
+    $('.item').on('click', '.delete' , deleteItem);
+    $('.list-form').keydown(function(e) {
+        if (e.which == 13) {
+            addListItem();
+        }
+  
+    });
+     
+    
+});
+
+// });
+
+// jquery convert integer to price
