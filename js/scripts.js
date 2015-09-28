4fcf3cd302bf3c4f7ca9e2b0513a914ccb9b190c
Maile Lucks <maile.lucks@gmail.com>
2015-09-28 14:05:28 -0700

    button not input for delete

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@
 		var itemRow = $('<div class="item"><ul class="item-details"><li>' + itemName + '</li><li>' + itemPrice +'</li><li>' + itemQuantity + '</li><li>' + itemRetailer + '</li></ul><ul class="options"><li class="check"><input class="check" type="checkbox" name="checked"></li><li class="switch"><input id="toggle" class="toggle toggle-round" type="checkbox"><label for="toggle"></label>	</li><li class="delete"><button id="delete">X</button></li></ul></div>' );
 
 		$('.shop-list').prepend(itemRow);
-    	$('#itemName').val();
+    	$('itemName').val();
 	}
 
 	/* Allows user to delete list items */
