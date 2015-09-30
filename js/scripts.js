7cc85720424ef26ebe22bc228f24070aedd693a2
Maile Lucks <maile.lucks@gmail.com>
2015-09-29 23:47:33 -0700

    Removing Toggle from App

--- js/scripts.js
+++ js/scripts.js
@@ -7,26 +7,32 @@
 		var itemPrice = $('#price').val();
 		var itemQuantity = $('#quantity').val();
 		var itemRetailer = $('#retailer').val();
-		var itemRow = $('<div class="item"><ul class="item-details"><li>' + itemName + '</li><li>' + itemPrice +'</li><li>' + itemQuantity + '</li><li>' + itemRetailer + '</li></ul><ul class="options"><li class="check"><input class="check" type="checkbox" name="checked"></li><li class="switch"><input id="toggle" class="toggle toggle-round" type="checkbox"><label for="toggle"></label>	</li><li class="delete"><button id="delete">X</button></li></ul></div>' );
+		var itemRow = $('<div class="item"><ul class="item-details"><li>' + itemName + '</li><li>' + "$" + itemPrice +'</li><li>' + itemQuantity + '</li><li>' + itemRetailer + '</li></ul><ul class="options"><li class="check"><input class="check" type="checkbox" name="checked"></li><li class="delete"><button id="delete">X</button></li></ul></div>' );
 
 		$('.shop-list').prepend(itemRow);
     	$('itemName').val();
 	}
 
-	/* Allows user to delete list items */
+/* Allows user to delete list items */
 function deleteItem() {
 
     $(this).parent().parent().remove();
+}
+
+/* Allows user to check list items */
+function checkItem() {
+
+	$(this).parent().parent().toggleClass('checked');
 }
 
 	/* Adds item to list */
 $(function() {
 
     $('#addItem').on('click', addListItem);
-    // $('#itemList').on('click', '.checkbox', tickItem);
-    $('.item').on('click', '.delete' , deleteItem);
+    $('.item').on('click', '.check', checkItem);
+    $('.item').on("click", "li.delete" , deleteItem);
     $('.list-form').keydown(function(e) {
-        if (e.which == 13) {
+        if (e.which == 13 ) {
             addListItem();
         }
   
